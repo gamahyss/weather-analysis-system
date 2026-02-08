903b67116ca177ad2642092017e2a09c0b4125fd
gamahyss <knuxko@MacBook-Air-Alexandra.local>
2026-02-08 17:47:47 +0300

    исправлены ошибки в fetcher

---
+++ backend/analyzer.py
@@ -0,0 +1,7 @@
+import pandas
+import fetcher
+import datetime
+
+def analyze_montly(df: pandas.DataFrame):
+    #должен выводить датафреймы всех погодных параметров по дням месяца и датафрейм со средними значениями по всем параметрам
+    return
--- backend/fetcher.py
+++ backend/fetcher.py
@@ -19,9 +19,7 @@ def fetch_weather_data(latitude, longitude, start_date, end_date):
     }
     response = requests.get(url, params=params)
     data = response.json()
-    
-    # Преобразуем JSON в DataFrame
-    df = pd.DataFrame(data["daily"])
+    df = pd.DataFrame(data)
     return df
 
 def get_geolocation(city):
@@ -34,38 +32,49 @@ def get_geolocation(city):
         raise ValueError(f"Город '{city}' не найден")
 
     latitude = round(data[2]['lat'], 2)
-    longtitude = round(data[2]['lon'], 2)
+    longitude = round(data[2]['lon'], 2)
     
-    return latitude, longtitude
+    return latitude, longitude
 
 def fetch_daily(city_name, date: datetime.datetime):
     lat, lon = get_geolocation(city_name)
     date_string = date.strftime("%Y-%m-%d")
-    return fetch_weather_data(lat, lon, date_string, date_string)
+    return fetch_weather_data(lat, lon, date_string, date_string)['daily']
 
 def fetch_monthly(city_name, begin_date: datetime.datetime):
     #ВНИМАНИЕ: в begin_date должна быть указана дата с ПЕРВЫМ ЧИСЛОМ данного месяца
     lat, lon = get_geolocation(city_name)
 
     date_string = begin_date.strftime("%Y-%m-%d")
-    year = date_string[0:4]
-    month = date_string[5:7]
-    days = monthrange(year, month)
-    end_date = begin_date + datetime.timedelta(days=days-1)
+    year = int(date_string[0:4])
+    month = int(date_string[5:7])
+    days = monthrange(year, month)[1]
+    end_date = begin_date + datetime.timedelta(days - 1)
 
-    df = fetch_weather_data(lat, lon, begin_date, end_date)
+    begin_date_str = begin_date.strftime("%Y-%m-%d")
+    end_date_str = end_date.strftime("%Y-%m-%d")
+
+    df = fetch_weather_data(lat, lon, begin_date_str, end_date_str)['daily']
     return df
 
-def analyze_yearly(city_name, begin_date: datetime.datetime):
+def fetch_yearly(city_name, begin_date: datetime.datetime):
     #ВНИМАНИЕ: в begin_date должна быть указана дата с ПЕРВЫМ ДНЁМ данного года
     lat, lon = get_geolocation(city_name)
 
     date_string = begin_date.strftime("%Y-%m-%d")
-    year = date_string[0:4]
+    year = int(date_string[0:4])
     days = 365
     if isleap(year):
-        days += 1
+        days = 366
     end_date = begin_date + datetime.timedelta(days - 1)
 
-    df = fetch_weather_data(lat, lon, begin_date, end_date)
+    begin_date_str = begin_date.strftime("%Y-%m-%d")
+    end_date_str = end_date.strftime("%Y-%m-%d")
+
+    df = fetch_weather_data(lat, lon, begin_date_str, end_date_str)['daily']
     return df
+
+#Проверка
+#print(fetch_daily('Moscow', datetime.datetime.strptime('2023-01-01', '%Y-%m-%d')))
+#print(fetch_monthly('Moscow', datetime.datetime.strptime('2023-01-01', '%Y-%m-%d')))
+#print(fetch_yearly('Moscow', datetime.datetime.strptime('2023-01-01', '%Y-%m-%d')))
